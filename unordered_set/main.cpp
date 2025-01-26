1b185d1e91c882898449dca700f58a504d5e4935
Elen Hayrapetyan <160489923+hayrapetyaann@users.noreply.github.com>
2025-01-26 21:10:01 +0400

    main function for hash set

---
+++ unordered_set/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "HashSet.h"
+
+int main() {
+    HashSet mySet(10);
+    mySet.insert(5);
+    mySet.insert(15);
+    mySet.insert(15);
+    mySet.insert(15);
+    mySet.insert(15);
+    mySet.insert(15);
+    mySet.insert(15);
+    mySet.insert(34);
+    mySet.insert(23);
+    mySet.insert(90);
+    mySet.insert(33);
+
+    mySet.print();
+    return 0;
+}
